a83308d3a54bf0787907ef23b97743478ae51cd5
Mohammad Kiani <kianimohammad@outlook.com>
2020-06-08 13:41:14 -0400

    initial commit

---
+++ TicTacToe-template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe-template
+//
+//  Created by Mohammad Kiani on 2020-06-08.
+//  Copyright © 2020 mohammadkiani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
